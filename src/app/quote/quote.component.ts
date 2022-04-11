8bc10347c6e0a020db3dc57eb544b732366a12ad
Dean Kago <martokhago@gmail.com>
2022-04-11 12:45:21 +0300

    added native element function

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,7 +10,7 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[]= [
-    new Quote(1, "", "", "it is never night in finland", 0, 0),
+    new Quote(1, "it is never night in finland", "", "", 0, 0),
     new Quote(2, "", "", "", 0, 0),
     new Quote(3, "", "", "", 0, 0),
     new Quote(4, "", "", "", 0, 0),
