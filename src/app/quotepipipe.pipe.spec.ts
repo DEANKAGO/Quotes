81f7b26dea74c23b270641099dc8b34cf1484dfb
Dean Kago <martokhago@gmail.com>
2022-04-11 23:57:12 +0300

    Auto-generated commit [ci skip]

--- src/app/quotepipipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { QuotepipipePipe } from './quotepipipe.pipe';
-
-describe('QuotepipipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new QuotepipipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
